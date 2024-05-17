6cdcaaa1d56549416f76e450be647f87ee71ab4f
yu-to06 <shun6.u@gmail.com>
2024-05-17 23:28:13 +0900

    edit ArticleList but Error occured at page.tsx

--- src/app/components/ArticleList.tsx
+++ src/app/components/ArticleList.tsx
@@ -1,78 +1,55 @@
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
+import {Article} from "../../types";
 
-const ArticleList = () => {
+type ArticleListProps = {
+  articles: Article[];
+};
+const ArticleList = ({articles}: ArticleListProps) => {
   return (
     <div>
-      <article className="shadow my-4">
-        <Link href="#" className="hover:opacity-75">
-          <Image
-            src="https://source.unsplash.com/collection/1346951/1000x500?sig=3"
-            alt=""
-            width={1280}
-            height={300}
-          ></Image>
-        </Link>
-        <div className="bg-white flex flex-col justify-start p-6">
-          <Link href="#" className="text-blue-700 pb-4 font-bold">
-            Technology
-          </Link>
-          <Link
-            href="#"
-            className="text-slate-500 text-3xl font-bold pb-4 hover:text-gray-700"
-          >
-            Next.js Learning
-          </Link>
-          <p className="text-sm pb-3 text-slate-500">
-            {" "}
-            Published on May 09 2024
-          </p>
-          <Link href="#" className="text-slate-500 pb-6">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum quae
-            vel cupiditate quod reprehenderit minus a enim sunt aliquam aperiam
-            voluptatem dolorem asperiores iure et consequatur, id autem. Rerum,
-            assumenda.
-          </Link>
-          <Link href="#" className="text-pink-500 hover:text-blue-700">
-            続きを読む
-          </Link>
-        </div>
-      </article>
-      <article className="shadow my-4">
-        <Link href="#" className="hover:opacity-75">
-          <Image
-            src="https://source.unsplash.com/collection/1346951/1000x500?sig=4"
-            alt=""
-            width={1280}
-            height={300}
-          ></Image>
-        </Link>
-        <div className="bg-white flex flex-col justify-start p-6">
-          <Link href="#" className="text-blue-700 pb-4 font-bold">
-            Technology
-          </Link>
-          <Link
-            href="#"
-            className="text-slate-500 text-3xl font-bold pb-4 hover:text-gray-700"
-          >
-            Next.js Learning
-          </Link>
-          <p className="text-sm pb-3 text-slate-500">
-            {" "}
-            Published on May 09 2024
-          </p>
-          <Link href="#" className="text-slate-500 pb-6">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum quae
-            vel cupiditate quod reprehenderit minus a enim sunt aliquam aperiam
-            voluptatem dolorem asperiores iure et consequatur, id autem. Rerum,
-            assumenda.
-          </Link>
-          <Link href="#" className="text-pink-500 hover:text-blue-700">
-            続きを読む
-          </Link>
-        </div>
-      </article>
+      {articles.map((article) => (
+        <>
+          <article className='shadow my-4' key={article.id}>
+            <Link href={`articles/${article.id}`} className='hover:opacity-75'>
+              <Image
+                src='https://source.unsplash.com/collection/1346951/1000x500?sig=3'
+                alt=''
+                width={1280}
+                height={300}
+              ></Image>
+            </Link>
+            <div className='bg-white flex flex-col justify-start p-6'>
+              <Link href='#' className='text-blue-700 pb-4 font-bold'>
+                Technology
+              </Link>
+              <Link
+                href={`articles/${article.id}`}
+                className='text-slate-500 text-3xl font-bold pb-4 hover:text-gray-700'
+              >
+                {article.title}
+              </Link>
+              <p className='text-sm pb-3 text-slate-500'>
+                {" "}
+                {article.createdAt}
+              </p>
+              <Link
+                href={`articles/${article.id}`}
+                className='text-slate-500 pb-6'
+              >
+                {article.content}
+              </Link>
+              <Link
+                href={`articles/${article.id}`}
+                className='text-pink-500 hover:text-blue-700'
+              >
+                続きを読む
+              </Link>
+            </div>
+          </article>
+        </>
+      ))}
     </div>
   );
 };
---
+++ src/blogAPI.ts
@@ -0,0 +1,9 @@
+import {Article} from "./types";
+
+export const getAllArticles = async (): Promise<Article[]> => {
+  // SSR
+  const res = await fetch("http://localhost:3001/posts", {cache: "no-store"});
+
+  const articles = await res.json();
+  return articles;
+};
